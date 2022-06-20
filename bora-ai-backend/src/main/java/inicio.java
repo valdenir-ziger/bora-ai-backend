2ffe9dd9f97e60684708f34e447d99deb886b309
valdenir-ziger <valdenir@alunos.utfpr.edu.br>
2022-06-20 13:55:35 -0300

    removido imports inuteis

--- bora-ai-backend/src/main/java/inicio.java
+++ bora-ai-backend/src/main/java/inicio.java
@@ -1,16 +1,10 @@
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
-
 import modulos.Usuario;
 
 public class inicio {
